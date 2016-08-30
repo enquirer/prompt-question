20a5dabf024f432831ce508e81997a399cf31b0c
jonschlinkert <jon.schlinkert@sellside.com>
2016-08-30 04:05:14 -0400

    adds `.choices` getter, code comments

--- index.js
+++ index.js
@@ -4,56 +4,159 @@ var Choices = require('prompt-choices');
 var utils = require('./lib/utils');
 
 /**
- * Create a new Question
+ * Create a new question with the given `name`, `message` and `options`.
+ *
+ * ```js
+ * var question = new Question('first', 'What is your first name?');
+ * console.log(question);
+ * // Question {
+ * //   type: 'input',
+ * //   name: 'color',
+ * //   message: 'What is your favorite color?'
+ * // }
+ * ```
+ * @param {String|Object} `name` Question name or options.
+ * @param {String|Object} `message` Question message or options.
+ * @param {String|Object} `options` Question options.
+ * @api public
  */
 
 function Question(name, message, options) {
+  if (arguments.length === 0) {
+    throw new TypeError('expected a string or object');
+  }
+
   if (utils.isObject(name) && name.isQuestion) {
     return name;
   }
-  this.isQuestion = true;
+
   this.type = 'input';
-  assign(this, {name: name, message: message, options: options});
-  if (this.choices) {
-    this.addChoices(this.choices);
-  }
+  utils.define(this, 'isQuestion', true);
+  utils.assign(this, {
+    name: name,
+    message: message,
+    options: options
+  });
 }
+
+/**
+ * Clone the question instance.
+ *
+ * ```js
+ * var clonedQuestion = question.clone();
+ * ```
+ * @return {Object} Returns the cloned question
+ * @api public
+ */
 
 Question.prototype.clone = function() {
   var cached = utils.clone(this.cache);
   return new this.constructor(cached);
 };
 
-Question.prototype.addChoices = function(choices, answers) {
-  this.choices = new Choices(choices, answers);
+/**
+ * Add formatted choice objects to the `question.choices` array.
+ * See [prompt-choices][] for more details.
+ *
+ * ```js
+ * question.addChoices(['foo', 'bar', 'baz']);
+ * ```
+ * @param {String|Array} `choices` One or more choices to add.
+ * @return {Object} Returns the question instance for chaining
+ * @api public
+ */
+
+Question.prototype.addChoices = function(choices) {
+  this.choices = choices;
   return this;
 };
+
+/**
+ * Toggle the `checked` value of the the choice at the given `idx`.
+ *
+ * ```js
+ * question.toggleChoice(1);
+ * ```
+ * @param {Number} `idx` The index of the choice to toggle.
+ * @return {Object} Returns the question instance for chaining
+ * @api public
+ */
+
+Question.prototype.toggleChoice = function(idx) {
+  if (typeof this.choices.toggleChoice === 'function') {
+    this.choices.toggleChoice(idx);
+  }
+  return this;
+};
+
+/**
+ * Enable the choice at the given `idx` and disable all other choices.
+ *
+ * ```js
+ * question.toggleChoices(3);
+ * ```
+ * @param {Number} `idx`
+ * @return {Object} Returns the question instance for chaining
+ * @api public
+ */
 
 Question.prototype.toggleChoices = function(idx) {
-  this.choices.toggleChoices(idx);
+  if (typeof this.choices.toggleChoices === 'function') {
+    this.choices.toggleChoices(idx);
+  }
   return this;
 };
 
-function assign(obj, options) {
-  options = options || {};
-  var cache = {};
-  for (var key in options) {
-    var val = options[key];
-    if (utils.isObject(val)) {
-      utils.extend(cache, val);
-    } else if (typeof val !== 'undefined') {
-      cache[key] = val;
-    }
-  }
-  cache.message = cache.message || cache.name;
-  cache.options = cache.options || {};
-  utils.extend(obj, cache);
-  utils.define(obj, 'cache', cache);
-}
+/**
+ * Returns the given `val` or `question.default` if `val` is undefined or null.
+ *
+ * ```js
+ * var question = new Question({name: 'first', message: 'First name'?, default: 'Bob'});
+ * console.log(question.getAnswer());
+ * //=> 'Bob'
+ * console.log(question.getAnswer('Joe'));
+ * //=> 'Joe'
+ * console.log(question.getAnswer(false));
+ * //=> false
+ * console.log(question.getAnswer(0));
+ * //=> 0
+ * ```
+ *
+ * @param {any} `val`
+ * @return {any}
+ * @api public
+ */
+
+Question.prototype.getAnswer = function(val) {
+  return (val != null && !!String(val)) ? val : (this.default || '');
+};
+
+/**
+ * Getter that returns true if a `default` value has been defined.
+ * @return {Boolean}
+ * @api public
+ */
 
 Object.defineProperty(Question.prototype, 'hasDefault', {
   get: function() {
     return this.default != null && !!String(this.default);
+  }
+});
+
+/**
+ * Getter that returns true if a `default` value has been defined.
+ * @return {Boolean}
+ * @api public
+ */
+
+Object.defineProperty(Question.prototype, 'choices', {
+  configurable: true,
+  enumerable: true,
+  set: function(choices) {
+    utils.define(this, '_choices', choices);
+  },
+  get: function() {
+    return new Choices(this._choices);
   }
 });
 
